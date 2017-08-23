d6bc9681f1376e57e8909d572294f11f3bda5afb
Higor Nucci <higornucci@gmail.com>
2017-08-23 13:50:42 -0400

    testando jacoco e pmd

--- src/main/java/br/com/alura/maven/App.java
+++ src/main/java/br/com/alura/maven/App.java
@@ -8,6 +8,9 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+
+        Produto produto = new Produto("Livro", 45);
+
+        System.out.println(produto.getNome() + " R$" + produto.getPreco());
     }
 }
